3ef0e00825d94c92c32e0ad444765e4ed8615327
avinmaster <oybek.odilov.dev@gmail.com>
2021-12-22 17:28:25 +0500

    Some refactoring

--- app/Http/Controllers/HouseController.php
+++ app/Http/Controllers/HouseController.php
@@ -19,18 +19,30 @@ class HouseController extends Controller
     public function search(Request $request): array
     {
         // checking for the presence of a filter or sorting type in the request and if available, using a filter or sorting 
-        $houses = House::when($request->name, function ($query, $name) {
-            return $query->where('name', 'like', "%{$name}%");
-        })->when($request->minPrice, function ($query, $minPrice) {
-            return $query->where('price', ">=", $minPrice);
-        })->when($request->maxPrice, function ($query, $maxPrice) {
-            return $query->where('price', "<=", $maxPrice);
-        // Sorting
-        })->when($request->sortByPrice && in_array($request->sortByPrice, ['m', 'l']), function ($query) use ($request) {
-            return $query->orderBy('price', $request->sortByPrice == 'l' ? 'asc' : 'desc');
-        }, function ($query) {
-            return $query->orderByDesc('id');
-        })->paginate(15);
+        $houses = House::when(
+            $request->name,
+            function ($query, $name) {
+                return $query->where('name', 'like', "%{$name}%");
+            }
+        )->when(
+            $request->minPrice,
+            function ($query, $minPrice) {
+                return $query->where('price', ">=", $minPrice);
+            }
+        )->when(
+            $request->maxPrice,
+            function ($query, $maxPrice) {
+                return $query->where('price', "<=", $maxPrice);
+            }
+        )->when( // Sorting
+            $request->sortByPrice && in_array($request->sortByPrice, ['m', 'l']),
+            function ($query) use ($request) {
+                return $query->orderBy('price', $request->sortByPrice == 'l' ? 'asc' : 'desc');
+            },
+            function ($query) {
+                return $query->orderByDesc('id');
+            }
+        )->paginate(15);
 
         // Return the search view with the results compacted
         return compact('houses');
